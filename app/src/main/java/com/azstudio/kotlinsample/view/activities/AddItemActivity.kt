35736966778105ef9f54649626939dc9e45021a9
athul <athul.6560@gmail.com>
2021-12-21 23:34:43 +0530

    removed warnings

--- app/src/main/java/com/azstudio/kotlinsample/view/activities/AddItemActivity.kt
+++ app/src/main/java/com/azstudio/kotlinsample/view/activities/AddItemActivity.kt
@@ -16,7 +16,7 @@ class AddItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
-    val sample:SampleData= SampleData(
+    val sample= SampleData(
         "sadff"
 
     )
