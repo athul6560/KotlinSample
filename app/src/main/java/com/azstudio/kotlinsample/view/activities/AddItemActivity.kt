5c64bdd2ab68e493812e6951033b336b5e6260ec
athul <athul.6560@gmail.com>
2021-12-21 22:25:53 +0530

    Initial commit

---
+++ app/src/main/java/com/azstudio/kotlinsample/view/activities/AddItemActivity.kt
@@ -0,0 +1,12 @@
+package com.azstudio.kotlinsample.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azstudio.kotlinsample.R
+
+class AddItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+}
